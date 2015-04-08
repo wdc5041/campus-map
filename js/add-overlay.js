61d00104c0bd718aada12186de0a05f36636539e
Aaron Dennis <aaron.p.dennis@gmail.com>
2015-04-08 00:29:20 -0400

    splitting up all that javascript + comments!

---
+++ js/add-overlay.js
@@ -0,0 +1,4 @@
+// Add overlay data to the map
+var overlay = L.mapbox.featureLayer() // create a new feature layer called overlay
+                .loadURL('./data/campus-map-overlay.json') // load in the campus-map-overlay GeoJSON file from the data folder
+                .addTo(map); // add overlay to map
