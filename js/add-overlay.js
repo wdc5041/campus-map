a17901a6ee998a2cdb2adeb6d890d1d999bd9a91
Aaron Dennis <aaron.p.dennis@gmail.com>
2015-04-08 11:55:33 -0400

    Update add-overlay.js

--- js/add-overlay.js
+++ js/add-overlay.js
@@ -1,4 +1,4 @@
 // Add overlay live information overlay data (like construction or alerts) to the map
 var overlay = L.mapbox.featureLayer() // create a new feature layer called overlay
-                .loadURL('./data/campus-map-overlay.json') // load in the campus-map-overlay GeoJSON file from the data folder
+                .loadURL('https://rawgit.com/pennstategeog467/campus-map/gh-pages/data/campus-map-overlay.json') // load in the campus-map-overlay GeoJSON file from the data folder
                 .addTo(map); // add overlay to map
